6b6291e18721a3f2fca7137fae0dc1cfdf92ab99
SerchAlex13 <sergio.rosales5262@alumnos.udg.mx>
2022-11-24 23:39:49 -0600

    Inicio proyecto hotel

--- resources/views/components/plantilla.blade.php
+++ resources/views/components/plantilla.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>DSA Diseños</title>
+    <title>Hotel Tapatío</title>
 
     @vite(['resources/css/bootstrap.css', 'resources/js/bootstrapjs.js', 'resources/css/style.css'])
 
